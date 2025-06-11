6e58ae70fc6d0b835eca198df26ef49d9d7ef987
amrm <afonso.muralha@spinworks.pt>
2025-06-11 10:47:44 +0100

    updated i3

--- install_utilities_ubuntu.sh
+++ install_utilities_ubuntu.sh
@@ -1,6 +1,10 @@
 
 
-sudo apt install lxpolkit flameshot bat zsh fzf ranger libssl-dev tmux git i3blocks -y
+sudo apt install policykit-desktop-privileges policykit-1-gnome flameshot bat zsh fzf ranger libssl-dev tmux git i3blocks -y
+
+
+
+
 
 sudo snap install lsd
 #sudo apt install wezterm -y
